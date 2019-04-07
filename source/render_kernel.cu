730d627352ec205c02746c50c1a8faa19c0c82a2
Sergen Eren <sergeneren@gmail.com>
2019-04-07 18:25:29 +0300

    atmosphere implementation

--- source/render_kernel.cu
+++ source/render_kernel.cu
@@ -29,6 +29,7 @@
 
 #include <stdio.h>
 #include "cuda_math.cuh"
+#include <float.h>
 #include <cuda_runtime.h> 
 #include <curand_kernel.h>
 
@@ -108,9 +109,9 @@ __device__ inline bool solveQuadratic(float a, float b, float c, float& x1, floa
 
 __device__ bool raySphereIntersect(const float3& orig, const float3& dir, const float& radius, float& t0, float& t1) {
 	
-	float A = dir.squared_length();
-	float B = 2 * (dir.x() * orig.x() + dir.y() * orig.y() + dir.z() * orig.z());
-	float C = orig.x() * orig.x() + orig.y() * orig.y() + orig.z() * orig.z() - radius * radius;
+	float A = squared_length(dir);
+	float B = 2 * (dir.x * orig.x + dir.y * orig.y + dir.z * orig.z);
+	float C = orig.x * orig.x + orig.y * orig.y + orig.z * orig.z - radius * radius;
 
 	if (!solveQuadratic(A, B, C, t0, t1)) return false;
 
@@ -122,6 +123,25 @@ __device__ bool raySphereIntersect(const float3& orig, const float3& dir, const 
 	return true;
 }
 
+__device__ inline float degree_to_radians(float degree) {
+
+	return degree * M_PI / 180.0f;
+
+}
+
+
+__device__ inline float3 degree_to_cartesian(float azimuth, float elevation) {
+
+
+	azimuth =degree_to_radians(clamp(azimuth, 0.0f, 360.0f));
+	elevation =degree_to_radians(clamp(elevation, .0f, 180.0f));
+		
+	float x = sinf(elevation) * cosf(azimuth);
+	float y = sinf(elevation) * sinf(azimuth);
+	float z = cosf(elevation);
+
+	return make_float3(x, y, z);
+}
 
 //Phase functions pdf 
 
@@ -265,6 +285,81 @@ __device__ inline bool integrate(
 
 }
 
+
+// Light Samplers 
+
+__device__ inline float3 sample_atmosphere(const Kernel_params &kernel_params ,const float3 orig, const float3 dir, float tmin, float tmax) {
+
+	// initial parameters
+	float	atmosphereRadius = 6420e3f;
+	float3	sunDirection = degree_to_cartesian(kernel_params.light_pos.x, kernel_params.light_pos.y);
+	float	earthRadius = 6360e3f;
+	float	Hr = 7994.0f;
+	float	Hm = 1200.0f;
+	float3	betaR = make_float3(3.8e-6f, 13.5e-6f, 33.1e-6f) ;
+	float3	betaM = make_float3(21e-6f);
+	//
+
+
+	float t0, t1;
+
+	if (!raySphereIntersect(orig, dir, atmosphereRadius, t0, t1) || t1 < 0) return make_float3(1.0f, .0f, .0f);
+	if (t0 > tmin && t0 > 0) tmin = t0;
+	if (t1 < tmax) tmax = t1;
+
+	uint numSamples = 16;
+	uint numSamplesLight = 8;
+
+	float segmentLength = (tmax - tmin) / numSamples;
+	float tCurrent = tmin;
+	float3 sumR = make_float3(0.0f, .0f, .0f); // Rayleigh contribution
+	float3 sumM = make_float3(0.0f, .0f, .0f); // Mie contribution
+
+	float opticalDepthR = 0, opticalDepthM = 0;
+	float mu = dot(dir, sunDirection); // mu in the paper which is the cosine of the angle between the sun direction and the ray direction
+	float phaseR = 3.f / (16.f * M_PI) * (1 + mu * mu);
+	float g = 0.76f;
+
+	float phaseM = 3.f / (8.f * M_PI) * ((1.f - g * g) * (1.f + mu * mu)) / ((2.f + g * g) * pow(1.f + g * g - 2.f * g * mu, 1.5f));
+
+	for (uint i = 0; i < numSamples; ++i) {
+		float3 samplePosition = orig + (tCurrent + segmentLength * 0.5f) * dir;
+		float height = length(samplePosition) - earthRadius;
+		// compute optical depth for light
+		float hr = exp(-height / Hr) * segmentLength;
+		float hm = exp(-height / Hm) * segmentLength;
+		opticalDepthR += hr;
+		opticalDepthM += hm;
+		// light optical depth
+		float t0Light, t1Light;
+		raySphereIntersect(samplePosition, sunDirection, atmosphereRadius, t0Light, t1Light);
+		float segmentLengthLight = t1Light / numSamplesLight, tCurrentLight = 0;
+		float opticalDepthLightR = 0, opticalDepthLightM = 0;
+		uint j;
+		for (j = 0; j < numSamplesLight; ++j) {
+			float3 samplePositionLight = samplePosition + (tCurrentLight + segmentLengthLight * 0.5f) * sunDirection;
+			float heightLight = length(samplePositionLight) - earthRadius;
+			if (heightLight < 0) break;
+			opticalDepthLightR += exp(-heightLight / Hr) * segmentLengthLight;
+			opticalDepthLightM += exp(-heightLight / Hm) * segmentLengthLight;
+			tCurrentLight += segmentLengthLight;
+		}
+		if (j == numSamplesLight) {
+			float3 tau = betaR * (opticalDepthR + opticalDepthLightR) + betaM * 1.1f * (opticalDepthM + opticalDepthLightM);
+			float3 attenuation = make_float3(exp(-tau.x), exp(-tau.y), exp(-tau.z));
+			sumR += attenuation * hr;
+			sumM += attenuation * hm;
+		}
+		tCurrent += segmentLength;
+	}
+
+
+	return (sumR * betaR * phaseR + sumM * betaM * phaseM) * 20 ;
+}
+
+
+// Volume functions 
+
 __device__ inline float3 transmittance(Rand_state &rand_state, float3 pos , const Kernel_params &kernel_params, VDBInfo &gvdb) {
 
 	float3 p = pos; 
@@ -358,7 +453,13 @@ extern "C" __global__ void volume_rt_kernel(VDBInfo gvdb, const Kernel_params ke
 	
 	float3 ray_dir =  normalize(getViewRay((float(scn.width - x) + 0.5) / scn.width, (float(scn.height - y) + 0.5) / scn.height));
 
-	float3 value = trace_volume(rand_state, scn.campos, ray_dir, kernel_params, gvdb);
+	float3 value;
+	float t0, t1, tmax = FLT_MAX;
+	float3 pos = scn.campos + 2000 + 6360e3f;
+	if (raySphereIntersect(pos, ray_dir, 6360e3f, t0, t1) && t1 > .0f) tmax = fmaxf(.0f, t0);
+		
+	value = sample_atmosphere(kernel_params ,pos, ray_dir, 0, tmax);
+	//float3 value = trace_volume(rand_state, scn.campos, ray_dir, kernel_params, gvdb);
 
 	// Accumulate.
 	if (kernel_params.iteration == 0)
