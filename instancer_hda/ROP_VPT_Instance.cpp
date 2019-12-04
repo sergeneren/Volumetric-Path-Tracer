5f03baec3909666ca5e571222b58bb2af384719d
Sergen Eren <sergeneren@gmail.com>
2019-12-04 11:15:40 +0300

    Converted SOP renderer to ROP renderer. Created main frame

--- instancer_hda/ROP_VPT_Instance.cpp
+++ instancer_hda/ROP_VPT_Instance.cpp
@@ -36,3 +36,247 @@
 //-----------------------------------------------
 
 
+#include "ROP_VPT_Instance.h"
+
+
+using namespace vpt_instance;
+
+
+
+
+static PRM_Name names[] = {
+	PRM_Name("outputFile",     "Output File"),
+	PRM_Name("soppath", "SOP Path")
+};
+
+static PRM_Default	 theFileDefault(0, "$HIP/Outputs/VPT/defgeo.json");
+const char *nameName = "`$OS`";
+static PRM_Default	 theNameDefault(0, nameName);
+
+
+PRM_Template VPT_INS_ROP::myTemplateList[] = {
+	PRM_Template(PRM_FILE_E,	1, &names[0], &theFileDefault , 0, 0 , 0 ,  &PRM_SpareData::fileChooserModeWrite),	// file Output
+	PRM_Template(PRM_STRING, PRM_TYPE_DYNAMIC_PATH, 1, &names[1], 0, 0, 0, 0, &PRM_SpareData::sopPath),			// sop path
+	PRM_Template()																									// placeholder
+};
+
+// End custom template list
+
+static PRM_Name         MKPATH_PRM_NAME("mkpath", "Create Intermediate Directories");
+static PRM_Name         INITSIM_PRM_NAME("initsim", "Initialize Simulation OPs");
+static PRM_Name         ALFPROGRESS_PRM_NAME("alfprogress", "Alfred Style Progress");
+
+static PRM_Template * getTemplates()
+{
+	static PRM_Template *prmTemplate = 0;
+
+	if (prmTemplate)
+	{
+		return prmTemplate;
+	}
+
+	prmTemplate = new PRM_Template[ROP_VPT_MAXPARMS + 1];
+
+	prmTemplate[ROP_VPT_RENDER] = theRopTemplates[ROP_RENDER_TPLATE];
+	prmTemplate[ROP_VPT_RENDERBACKGROUND] = theRopTemplates[ROP_RENDERBACKGROUND_TPLATE];
+	prmTemplate[ROP_VPT_RENDER_CTRL] = theRopTemplates[ROP_RENDERDIALOG_TPLATE];
+	prmTemplate[ROP_VPT_TRANGE] = theRopTemplates[ROP_TRANGE_TPLATE];
+	prmTemplate[ROP_VPT_FRANGE] = theRopTemplates[ROP_FRAMERANGE_TPLATE];
+	prmTemplate[ROP_VPT_TAKE] = theRopTemplates[ROP_TAKENAME_TPLATE];
+	prmTemplate[ROP_VPT_SOPOUTPUT] = VPT_INS_ROP::myTemplateList[0];
+	prmTemplate[ROP_VPT_SOPPATH] = VPT_INS_ROP::myTemplateList[1];
+
+	prmTemplate[ROP_VPT_TPRERENDER] = theRopTemplates[ROP_TPRERENDER_TPLATE];
+	prmTemplate[ROP_VPT_PRERENDER] = theRopTemplates[ROP_PRERENDER_TPLATE];
+	prmTemplate[ROP_VPT_LPRERENDER] = theRopTemplates[ROP_LPRERENDER_TPLATE];
+	prmTemplate[ROP_VPT_TPREFRAME] = theRopTemplates[ROP_TPREFRAME_TPLATE];
+	prmTemplate[ROP_VPT_PREFRAME] = theRopTemplates[ROP_PREFRAME_TPLATE];
+	prmTemplate[ROP_VPT_LPREFRAME] = theRopTemplates[ROP_LPREFRAME_TPLATE];
+	prmTemplate[ROP_VPT_TPOSTFRAME] = theRopTemplates[ROP_TPOSTFRAME_TPLATE];
+	prmTemplate[ROP_VPT_POSTFRAME] = theRopTemplates[ROP_POSTFRAME_TPLATE];
+	prmTemplate[ROP_VPT_LPOSTFRAME] = theRopTemplates[ROP_LPOSTFRAME_TPLATE];
+	prmTemplate[ROP_VPT_TPOSTRENDER] = theRopTemplates[ROP_TPOSTRENDER_TPLATE];
+	prmTemplate[ROP_VPT_POSTRENDER] = theRopTemplates[ROP_POSTRENDER_TPLATE];
+	prmTemplate[ROP_VPT_LPOSTRENDER] = theRopTemplates[ROP_LPOSTRENDER_TPLATE];
+	prmTemplate[ROP_VPT_MKPATH] = theRopTemplates[ROP_MKPATH_TPLATE];
+	prmTemplate[ROP_VPT_INITSIM] = theRopTemplates[ROP_INITSIM_TPLATE];
+	prmTemplate[ROP_VPT_ALFPROGRESS] = PRM_Template(PRM_TOGGLE, 1, &ALFPROGRESS_PRM_NAME, PRMzeroDefaults);
+
+	prmTemplate[ROP_VPT_MAXPARMS] = PRM_Template();
+
+	return prmTemplate;
+};
+
+OP_TemplatePair * VPT_INS_ROP::getTemplatePair()
+{
+	static OP_TemplatePair *ropPair = 0;
+
+	if (ropPair)
+	{
+		return ropPair;
+	}
+
+	ropPair = new OP_TemplatePair(getTemplates());
+
+	return ropPair;
+};
+
+OP_VariablePair * VPT_INS_ROP::getVariablePair()
+{
+	static OP_VariablePair *varPair = 0;
+
+	if (varPair)
+	{
+		return varPair;
+	}
+
+	varPair = new OP_VariablePair(ROP_Node::myVariableList);
+
+	return varPair;
+};
+
+// start, end and render frames are auto invoked by houdini
+
+int VPT_INS_ROP::startRender(int /*nframes*/, fpreal tstart, fpreal tend)
+{
+	int			 rcode = 1;
+
+	endTime = tend;
+	startTime = tstart;
+
+	if (INITSIM())
+	{
+		initSimulationOPs();
+		OPgetDirector()->bumpSkipPlaybarBasedSimulationReset(1);
+	}
+
+	if (error() < UT_ERROR_ABORT)
+	{
+		if (!executePreRenderScript(tstart))
+			return 0;
+	}
+
+	return rcode;
+}
+
+
+ROP_RENDER_CODE VPT_INS_ROP::renderFrame(fpreal time, UT_Interrupt *)
+{
+
+	SOP_Node		*sop;
+	UT_String		 soppath, savepath, name;
+
+	OUTPUT(savepath, time);
+	//NAME(name, time);
+
+	if (!executePreFrameScript(time))
+		return ROP_ABORT_RENDER;
+
+	// From here, establish the SOP that will be rendered, if it cannot
+	// be found, return 0.
+	// This is needed to be done here as the SOPPATH may be time
+	// dependent (ie: OUT$F) or the perframe script may have
+	// rewired the input to this node.
+
+	sop = CAST_SOPNODE(getInput(0));
+	if (sop)
+	{
+		// If we have an input, get the full path to that SOP.
+		sop->getFullPath(soppath);
+	}
+	else
+	{
+		// Otherwise get the SOP Path from our parameter.
+		SOPPATH(soppath, time);
+	}
+
+	if (!soppath.isstring())
+	{
+		addError(ROP_MESSAGE, "Invalid SOP path");
+		return ROP_ABORT_RENDER;
+	}
+
+	sop = getSOPNode(soppath, 1);
+	if (!sop)
+	{
+		addError(ROP_COOK_ERROR, (const char *)soppath);
+		return ROP_ABORT_RENDER;
+	}
+	OP_Context		context(time);
+	GU_DetailHandle gdh;
+	gdh = sop->getCookedGeoHandle(context);
+
+	GU_DetailHandleAutoReadLock	 gdl(gdh);
+	const GU_Detail		*gdp = gdl.getGdp();
+
+	if (!gdp)
+	{
+		addError(ROP_COOK_ERROR, (const char *)soppath);
+		return ROP_ABORT_RENDER;
+	}
+
+	if (evalInt("mkpath", 0, 0)) {
+		ROP_Node::makeFilePathDirs(savepath);
+	}
+
+	//h2a_fileSave(gdp, (const char *)savepath, (const char *)name, PWIDTH(time), SHUTTER(time), MODE(), MOTIONB(), COLOR(), TYPE(), P_RENDER_TYPE(), RADIUS(time), SUBDIV_TYPE(), SUBDIV_ITE(time));
+
+	if (ALFPROGRESS() && (endTime != startTime))
+	{
+		fpreal		fpercent = (time - startTime) / (endTime - startTime);
+		int		percent = (int)SYSrint(fpercent * 100);
+		percent = SYSclamp(percent, 0, 100);
+		fprintf(stdout, "ALF_PROGRESS %d%%\n", percent);
+		fflush(stdout);
+	}
+
+	if (error() < UT_ERROR_ABORT)
+	{
+		if (!executePostFrameScript(time))
+			return ROP_ABORT_RENDER;
+	}
+
+	return ROP_CONTINUE_RENDER;
+}
+
+ROP_RENDER_CODE VPT_INS_ROP::endRender()
+{
+	if (INITSIM())
+		OPgetDirector()->bumpSkipPlaybarBasedSimulationReset(-1);
+
+	if (error() < UT_ERROR_ABORT)
+	{
+		if (!executePostRenderScript(endTime))
+			return ROP_ABORT_RENDER;
+	}
+	return ROP_CONTINUE_RENDER;
+}
+
+VPT_INS_ROP::~VPT_INS_ROP(){}
+
+
+OP_Node * VPT_INS_ROP::nodeConstructor(OP_Network *net, const char *name, OP_Operator *op)
+{
+	return new VPT_INS_ROP(net, name, op);
+};
+
+
+VPT_INS_ROP::VPT_INS_ROP(OP_Network *net, const char *name, OP_Operator *op)
+
+	:ROP_Node(net, name, op)
+	, endTime(0)
+
+{};
+
+
+void newDriverOperator(OP_OperatorTable *table)
+{
+	table->addOperator(new OP_Operator("VPT_Instance",
+		"VPT Instance",
+		VPT_INS_ROP::nodeConstructor,
+		VPT_INS_ROP::getTemplatePair(),
+		0,
+		0,
+		VPT_INS_ROP::getVariablePair(),
+		OP_FLAG_GENERATOR));
+};
--- instancer_hda/ROP_VPT_Instance.h
+++ instancer_hda/ROP_VPT_Instance.h
@@ -36,3 +36,126 @@
 //-----------------------------------------------
 
 
+#ifndef _ROP_VPT_INSTANCE_H_
+#define _ROP_VPT_INSTANCE_H_
+
+
+#include <UT/UT_String.h>
+#include <UT/UT_Vector2.h>
+#include <UT/UT_Vector3.h>
+#include <UT/UT_Vector4.h>
+#include <UT/UT_Interrupt.h>
+#include <UT/UT_DSOVersion.h>
+
+#include <ROP/ROP_Node.h>
+#include <ROP/ROP_Error.h>
+#include <ROP/ROP_Templates.h>
+#include <ROP/ROP_API.h>
+#include <ROP/ROP_Error.h>
+
+#include <PRM/PRM_Include.h>
+#include <PRM/PRM_SpareData.h>
+#include <PRM/PRM_ChoiceList.h>
+
+#include <OP/OP_Operator.h>
+#include <OP/OP_Director.h>
+#include <OP/OP_OperatorTable.h>
+#include <OP/OP_AutoLockInputs.h>
+
+#include <SOP/SOP_Node.h>
+
+#include <GU/GU_Detail.h>
+
+#include <GA/GA_AIFTuple.h>
+#include <GA/GA_Iterator.h>
+#include <GA/GA_Types.h>
+#include <GA/GA_AttributeFilter.h>
+
+#include <SYS/SYS_Math.h>
+
+
+
+namespace vpt_instance {
+
+
+	enum {
+		ROP_VPT_RENDER,
+		ROP_VPT_RENDERBACKGROUND,
+		ROP_VPT_RENDER_CTRL,
+		ROP_VPT_TRANGE,
+		ROP_VPT_FRANGE,
+		ROP_VPT_TAKE,
+		ROP_VPT_SOPPATH,
+		ROP_VPT_SOPOUTPUT,
+
+		//render parameters
+		ROP_VPT_INITSIM,
+		ROP_VPT_MKPATH,
+		ROP_VPT_ALFPROGRESS,
+		ROP_VPT_TPRERENDER,
+		ROP_VPT_PRERENDER,
+		ROP_VPT_LPRERENDER,
+		ROP_VPT_TPREFRAME,
+		ROP_VPT_PREFRAME,
+		ROP_VPT_LPREFRAME,
+		ROP_VPT_TPOSTFRAME,
+		ROP_VPT_POSTFRAME,
+		ROP_VPT_LPOSTFRAME,
+		ROP_VPT_TPOSTRENDER,
+		ROP_VPT_POSTRENDER,
+		ROP_VPT_LPOSTRENDER,
+
+		ROP_VPT_MAXPARMS
+	};
+
+
+	class VPT_INS_ROP : public ROP_Node {
+
+
+	public:
+		static OP_TemplatePair      *getTemplatePair();
+		static OP_VariablePair      *getVariablePair();
+		static PRM_Template			myTemplateList[];
+		static OP_Node              *nodeConstructor(OP_Network *net, const char *name, OP_Operator *op);
+
+
+	protected:
+
+		VPT_INS_ROP(OP_Network *net, const char *name, OP_Operator *op);
+
+		virtual ~VPT_INS_ROP();
+
+
+
+	protected:
+
+		virtual int startRender(int nframes, fpreal s, fpreal e);
+		virtual ROP_RENDER_CODE renderFrame(fpreal time, UT_Interrupt *boss);
+		virtual ROP_RENDER_CODE endRender();
+
+
+	private:
+		void	OUTPUT(UT_String &str, fpreal t) { return evalString(str, "outputFile", 0, t); }
+		void	SOPPATH(UT_String &str, fpreal t) { return evalString(str, "soppath", 0, t); }
+		int		INITSIM() { return evalInt("initsim", 0, 0); }
+		int		ALFPROGRESS() { return evalInt("alfprogress", 0, 0); }
+
+
+		fpreal  startTime;
+		fpreal  endTime;
+
+	};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+#endif // !_ROP_VPT_INSTANCE_H_
